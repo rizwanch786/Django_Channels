74993ce44716884216214f0f095d62c287b77070
rizwanch786 <rizwangcsit@gmial.com>
2021-09-03 16:10:19 +0500

    commit

--- home/models.py
+++ home/models.py
@@ -20,3 +20,11 @@ class Notification(models.Model):
                 'value' : json.dumps(data)
             })
         super(Notification, self).save(*args, **kwargs)
+
+    class Student(models.Model):
+        student_name = models.CharField(max_length=100)
+        student_email = models.EmailField(max_length=100)
+        address = models.CharField(max_length=100)
+        age  = models.IntegerField()
+        def __str__(self):
+            return self.student_name
